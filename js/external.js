3f3938603c450e933434e44fea9b86f0d59d2802
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-07-06 11:50:19 -0500

    created js folder w/ external.js; created javascript-and-html.html

---
+++ js/external.js
@@ -0,0 +1,6 @@
+/*use strict.*/
+// prompt
+
+var currentSpeedInMph = 65;
+var plannedSpeedInMph = prompt("How fast do you plan to go in mph?");
+var distanceLeftInMiles = prompt("How many miles away are you from your destination?");
