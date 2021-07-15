63ace20df6f40364c9bbaef2897df735365263b9
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-07-15 14:19:55 -0500

    updated for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,20 @@
-function showMultiplicationTable() {
+/**function showMultiplicationTable() {
     var x = prompt("What is the multiplicand?")
     for (var i = 1; i <=10; i++) {
         console.log(x + "*" + i + "=" + x * i)
     }
 }
-showMultiplicationTable();
+showMultiplicationTable();**/
 
+function isOddEven() {
+        for (var i = 1 ; i <= 10; i++) {
+            var randNum;
+            randNum = Math.floor(Math.random() * 180) + 20;
+            if (randNum % 2 !== 0) {
+                console.log(randNum + " is " + " odd.");
+            }if (randNum % 2 === 0) {
+                console.log(randNum + " is " + " even.");
+            }
+            console.log(i)}
+}
+isOddEven();
