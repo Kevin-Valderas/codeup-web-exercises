5a8e3d568f6d39cd197bd74c53245b643ce3a6ad
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-07-15 12:03:22 -0500

    created for_loops.js; updated loops.html

---
+++ js/for_loops.js
@@ -0,0 +1,8 @@
+function showMultiplicationTable() {
+    var x = prompt("What is the multiplicand?")
+    for (var i = 1; i <=10; i++) {
+        console.log(x + "*" + i + "=" + x * i)
+    }
+}
+
+console.log(showMultiplicationTable())
