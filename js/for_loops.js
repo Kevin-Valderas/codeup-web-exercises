388fcf327ed175313857876376cf4271d0788c47
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-07-15 15:31:01 -0500

    updated for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -6,15 +6,30 @@
 }
 showMultiplicationTable();**/
 
-function isOddEven() {
-        for (var i = 1 ; i <= 10; i++) {
-            var randNum;
-            randNum = Math.floor(Math.random() * 180) + 20;
-            if (randNum % 2 !== 0) {
-                console.log(randNum + " is " + " odd.");
-            }if (randNum % 2 === 0) {
-                console.log(randNum + " is " + " even.");
-            }
-            console.log(i)}
+/**function isOddEven() {
+    for (var i = 1; i <= 10; i++) {
+        var randNum;
+        randNum = Math.floor(Math.random() * 180) + 20;
+        if (randNum % 2 !== 0) {
+            console.log(randNum + " is " + " odd.");
+        }
+        if (randNum % 2 === 0) {
+            console.log(randNum + " is " + " even.");
+        }
+        console.log(i)
+    }
 }
-isOddEven();
+isOddEven();**/
+
+function makePyramid(x) {
+    for (i = 1; i <= x; i++) {
+        var makeString = "";
+        for (var j = 0; j < i; j++) {
+            makeString += i.toString();
+        }
+        console.log(makeString);
+    }
+} makePyramid(9)
+
+
+
