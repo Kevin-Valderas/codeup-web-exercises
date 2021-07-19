08515a79e660274034a4e035a1a0108a0898cc23
Kevin Valderas <kevin.d.valderas34@gmail.com>
2021-07-19 12:15:27 -0500

    updated planets-array.js

--- js/planets-array.js
+++ js/planets-array.js
@@ -11,30 +11,52 @@
         'Uranus',
         'Neptune'
     ];
+console.log(planets);
 
     /**
      * TODO:
      * Read each console log below, and write some javascript code to perform
      * the step that it describes
      */
-
+function addSun(){
     console.log('Adding "The Sun" to the beginning of the planets array.');
-    console.log(planets);
-
+    return planets.unshift('The Sun');
+    }
+    addSun();
+    console.log(addSun());
+function addPluto(){
     console.log('Adding "Pluto" to the end of the planets array.');
-    console.log(planets);
+    return planets.push('Pluto');
+}
 
+    console.log(addPluto());
+function clearSun() {
     console.log('Removing "The Sun" from the beginning of the planets array.');
-    console.log(planets);
-
+    return planets.shift();
+}
+clearSun();
+    console.log(clearSun());
+function clearPluto() {
     console.log('Removing "Pluto" from the end of the planets array.');
-    console.log(planets);
-
+    return planets.pop();
+}
+    console.log(clearPluto());
+function index(input) {
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    return planets.indexOf(input);
+}
+console.log(index('Earth'));
 
     console.log("Reversing the order of the planets array.");
-    console.log(planets);
+    function reverse(){
+    return planets.reverse()
+    }
+
+    console.log(reverse());
 
     console.log("Sorting the planets array.");
-    console.log(planets);
-})();
+    function sort() {
+        return planets.sort();
+    }
+    console.log(sort());
+})();A
